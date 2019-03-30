34c0de95da0c2c33f7fbdd41f1166a643d6f4dba
phmstulp <phmstulp@minha.fag.edu.br>
2019-03-29 22:25:56 -0300

    PageNotFound criado, componentes do material adicionados e assets add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule, MatRadioModule, MatButtonModule, 
+  MatTableModule, MatIconModule, MatIconRegistry, MatPaginatorModule,
+   MatSort, MatSortModule } from '@angular/material';
+import { HttpClientModule, HttpClient } from '../../node_modules/@angular/common/http';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
+    BrowserAnimationsModule, //Daqui pra baixo são componentes do material
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
