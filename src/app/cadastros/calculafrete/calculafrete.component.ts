57d8721f9e8e3328995174a8059a592720134aaf
phmstulp <phmstulp@minha.fag.edu.br>
2019-04-13 15:34:26 -0300

    Tela de Cadastro de ValorFrete Criada

--- src/app/cadastros/calculafrete/calculafrete.component.ts
+++ src/app/cadastros/calculafrete/calculafrete.component.ts
@@ -3,6 +3,7 @@ import { Estado } from './models/estado';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '../../../../node_modules/@angular/material/paginator';
 import { MatSort } from '../../../../node_modules/@angular/material/sort';
+import { ValorFrete } from './models/valorfrete';
 
 @Component({
   selector: 'app-calculafrete',
@@ -12,11 +13,16 @@ import { MatSort } from '../../../../node_modules/@angular/material/sort';
 export class CalculafreteComponent implements OnInit {
 
   public estado: Estado;
-  public isExpandido: number;
+  public valorFrete: ValorFrete;
+  public isExpandidoEstado: number;
+  public isExpandidoValorFrete: number;
   public estadoList: Array<Estado>;
+  public valorFreteList: Array<ValorFrete>;
   public dsEstado: any;
+  public dsValorfrete: any;
 
-  displayedColumns: string[] = ['actionsColumn', 'id', 'sigla', 'nome'];
+  displayedColumnsEstado: string[] = ['actionsColumn', 'id', 'sigla', 'nome'];
+  displayedColumnsValorFrete: string[] = ['actionsColumn', 'id', 'estadoOrigem', 'estadoDestino', 'valor'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -24,13 +30,20 @@ export class CalculafreteComponent implements OnInit {
 
   ngOnInit() {
     this.estado = new Estado;
+    this.valorFrete = new ValorFrete;
     this.estadoList = new Array<Estado>();
-    this.isExpandido = 0;
+    this.valorFreteList = new Array<ValorFrete>();
+    this.isExpandidoEstado = 0;
+    this.isExpandidoValorFrete = 0;
     this.carregaEstado();
   }
 
-  setExpandido() {
-    this.isExpandido = 1;
+  setExpandidoEstado() {
+    this.isExpandidoEstado = 1;
+  }
+
+  setExpandidoValorFrete() {
+    this.isExpandidoValorFrete = 1;
   }
 
   salvarEstado(){
@@ -41,23 +54,31 @@ export class CalculafreteComponent implements OnInit {
     console.log(this.estadoList);
     this.estado = new Estado(); //Instancia uma novo Estado para não perder a referência da primeira0,
 
-    this.atualizaTable();   
+    this.atualizaTableEstado();   
+  }
+
+  salvarValorFrete(){
+    
   }
 
   carregaEstado(){
-    this.estado = new Estado;
-    this.estado.id = Math.floor(Math.random() * 100) + 1;
-    this.estado.nome = "Jooj";
-    this.estado.sigla = "JS";
-    this.estadoList.push(this.estado);
-    this.dsEstado = new MatTableDataSource<Estado>(this.estadoList);
+    //this.estado = new Estado;
+    //this.estado.id = Math.floor(Math.random() * 100) + 1;
+    //this.estado.nome = "Jooj";
+    //this.estado.sigla = "JS";
+    //this.estadoList.push(this.estado);
+    //this.dsEstado = new MatTableDataSource<Estado>(this.estadoList);
   }
 
-  sortData(){
+  sortDataEstado(){
     this.dsEstado.sort = this.sort;
   }
 
-  aplicarFiltro(valor: string){
+  sortDataValorFrete(){
+    this.dsValorfrete.sort = this.sort;
+  }
+
+  aplicarFiltroEstado(valor: string){
     valor = valor.trim();
     valor = valor.toLowerCase();
     console.log("Realiza o filtro com " + valor);
@@ -67,6 +88,10 @@ export class CalculafreteComponent implements OnInit {
       data.nome.toLowerCase().indexOf(filter) != -1;
   
     this.dsEstado.filter = valor;
+  }
+
+  aplicarFiltroValorFrete(valor: string) {
+
   }
 
   editarEstado(id: number) {
@@ -80,18 +105,36 @@ export class CalculafreteComponent implements OnInit {
     this.estado = estadoUpdate;
   }
 
+  editarValorFrete(id: number) {
+
+  }
+
   excluirEstado(id: number) {
     this.estadoList.splice(this.estadoList.findIndex
       (d => d.id === id), 1);
     console.log("Lista de Veiculos");
     console.log(this.estadoList);
-    this.atualizaTable();
+    this.atualizaTableEstado();
   }
 
-  atualizaTable() {
+  excluirValorFrete() {
+
+  }
+
+  atualizaTableEstado() {
     this.dsEstado = new MatTableDataSource<Estado>(this.estadoList);
     this.dsEstado.paginator = this.paginator;
     this.dsEstado.sort = this.sort;
+  }
+
+  atualizaTableValorFrete() {
+    this.dsValorfrete = new MatTableDataSource<ValorFrete>(this.valorFreteList);
+    this.dsValorfrete.paginator = this.paginator;
+    this.dsValorfrete.sort = this.sort;
+  }
+
+  atualizarEstadoOrigem() {
+
   }
 
 }
